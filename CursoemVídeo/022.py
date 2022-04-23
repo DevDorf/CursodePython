a16384e9d83401e8d4d04a3619c36e2ed8d40c7c
Caio Rodrigues <caiorodrigues1989@gmail.com>
2022-04-23 12:35:10 -0300

    add repository of Curso em Vídeo

---
+++ CursoemVídeo/022.py
@@ -0,0 +1,12 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Seu nome em maiusculas é {}'.format(nome.upper()))
+print('Seu nome em minusculas é {}'.format(nome.lower()))
+dividido = nome.split()
+print('Seu nome te ao todo {} letras.'.format(len(nome) - nome.count(' '))))
+print('Seu primeiro nome é {} e ele tem {} letras'.format(dividido[0], len(dividido[0])))
+
+
+
+
+
+
--- CursoemVídeo/Teste de Exercícios.py
+++ CursoemVídeo/Teste de Exercícios.py
@@ -1,2 +1,13 @@
-nome = str(input('Digite seu nome comprelo:'))
-print(nome.upper())
+A = float(input())
+B = float(input())
+C = float(input())
+tri = (A*C)/2
+cir = 3.14159*C**2
+tra = ((A + B) * C)/2
+qua = B*B
+ret = A*B
+print('TRIANGULO: {:.3f}'.format(tri))
+print('CIRCULO: {:.3f}'.format(cir))
+print('TRAPEZIO: {:.3f}'.format(tra))
+print('QUADRADO: {:.3f}'.format(qua))
+print('RETANGULO: {:.3f}'.format(ret))
