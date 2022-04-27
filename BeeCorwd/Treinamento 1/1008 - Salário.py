efc5a5bf573d778af49446841a6ac7251d8ffb70
Caio Rodrigues <caiorodrigues1989@gmail.com>
2022-04-27 20:26:49 -0300

    add repository Beecrowd

---
+++ BeeCorwd/Treinamento 1/1008 - Salário.py
@@ -0,0 +1,6 @@
+funcnum = int(input())
+hourjob = int(input())
+payhour = float(input())
+finalpay = hourjob*payhour
+print('NUMBER = {}'.format(funcnum))
+print('SALARY = U$ {:.2f}'.format(finalpay))
---
+++ BeeCorwd/Treinamento 1/1009 - Salário com Bônus.py
@@ -0,0 +1,6 @@
+nome = str(input())
+salariofinal = float(input())
+vendas = float(input())
+comicao = vend * (15/100)
+total = salariofinal + comicao
+print('TOTAL = R$ {:.2f}'.format(total))
