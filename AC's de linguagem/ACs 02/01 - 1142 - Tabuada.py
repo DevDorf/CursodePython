79b470546f517930009d8081e985e86d4589db84
Caio Rodrigues <caiorodrigues1989@gmail.com>
2022-05-12 21:17:46 -0300

    Resolução exercícios ac

---
+++ AC's de linguagem/ACs 02/01 - 1142 - Tabuada.py
@@ -0,0 +1,4 @@
+num = int(input())
+for c in range(1,11):
+    if 0 <= num <= 10:
+        print(f'{num} x {c} = {num * c}')
---
+++ AC's de linguagem/ACs 02/02 - 1164 - Triangulo Alfabético.py
@@ -0,0 +1,5 @@
+num = int(input())
+alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+for c in range(0, num):
+    print(f'{alfabeto[c] * (c + 1)}')
+    c += 1
