6d01e8d8ed00c6779379900643ffd8394bb9310b
Caio Rodrigues <caiorodrigues1989@gmail.com>
2022-05-14 11:40:56 -0300

    Refatoração de código

--- BeeCorwd/Treinamento 2/1040 - Média 3.py
+++ BeeCorwd/Treinamento 2/1040 - Média 3.py
@@ -1,29 +1,35 @@
+def media_aluno(n1, n2, n3, n4):
+    return (n1*2 + n2*3 + n3*4 + n4)/10
+
+def aluno_aprovado():
+    print('Aluno aprovado.')
+
+def aluno_reprovado():
+    print('Aluno reprovado.')
+
+def aluno_exame(media):
+    print('Aluno em exame.')
+    exame = float(input())
+    print(f'Nota do exame: {exame:.1f}')
+    media = (media + exame) / 2
+    if media >= 5.0:
+        aluno_aprovado()
+    else:
+        print('Aluno reprovado.')
+    print(f'Media final: {media:.1f}')
+
 n1, n2, n3, n4 = (input().split())
 n1 = float(n1)
 n2 = float(n2)
 n3 = float(n3)
 n4 = float(n4)
 
-media = ((n1*2 + n2*3 + n3*4 + n4*1)/(2 + 3 + 4 + 1))
+media = media_aluno(n1, n2, n3, n4)
+print(f'Media: {media:.1f}')
 
 if media >= 7.0:
-    print('Media: {:.1f}'.format(media))
-    print('Aluno aprovado.')
-elif media >= 5.0 and media <= 6.9:
-    print('Media: {:.1f}'.format(media))
-    print('Aluno em exame.')
-    exame = float(input())
-    print('Nota do exame: {:.1f}'.format(exame))
-    media2 = (media + exame)/2
-
-    if media2 >= 5.0:
-        print('Aluno aprovado.')
-        print('Media final: {:.1f}'.format(media2))
-    else:
-        print('Aluno reprovado.')
-        print('Media final: {:.1f}'.format(media2))
+    aluno_aprovado()
+elif media < 5:
+    aluno_reprovado()
 else:
-    print('Media: {:.1f}'.format(media))
-    print('Aluno reprovado.')
-
-
+    aluno_exame(media)
--- CursoemVídeo/Mundo 2/Exercicio 060.py
+++ CursoemVídeo/Mundo 2/Exercicio 060.py
@@ -1,3 +1,4 @@
+# Faça um programa que leia um numero qualquer e mostre o seu fatorial:
 num = int(input('Digite um valor: '))
 fatorial = (num * (num - 1) * (num - 2))
 print(fatorial)
