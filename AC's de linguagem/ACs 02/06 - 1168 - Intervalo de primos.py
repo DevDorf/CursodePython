91a582ce363f15cb5d42b5b453252e0a7246a278
Caio Rodrigues <caiorodrigues1989@gmail.com>
2022-05-16 11:55:31 -0300

    exercicios acs

--- AC's de linguagem/ACs 02/06 - 1168 - Intervalo de primos.py
+++ AC's de linguagem/ACs 02/06 - 1168 - Intervalo de primos.py
@@ -1,9 +1,9 @@
 inicio = int(input())
 fim = int(input())
 primo = 0
-while inicio < fim + 1:
-    if inicio % inicio == 0:
+for c in range(inicio, fim + 1):
+    if inicio % c == 0:
         print(inicio)
         primo += 1
-    inicio += 1
+        inicio += 1
 print('primos: ', primo)
