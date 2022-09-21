9690bd930f31a9401d63e7394be5553266fefe12
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2022-09-21 20:03:33 -0300

    Update teste.py

--- CursoemVídeo/teste.py
+++ CursoemVídeo/teste.py
@@ -1,4 +1,14 @@
-A = int(input())
-B = int(input())
-X = A + B
-print('X = {}'.format(X))
+cont7 = 0
+cont8 = 0
+sete = 1106
+oito = 1115
+while sete <= 1500:
+    sete += 7
+    cont7 += 1
+print(cont7)
+while oito <= 1500:
+    oito += 7
+    cont8 += 1
+print(cont8)
+print(soma = cont7 + cont8)
+
